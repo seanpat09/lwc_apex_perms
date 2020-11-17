1d72f41c355c22f5e20388541ef205a698d9cd5d
Sean Cuevo <scuevo@salesforce.com>
2020-11-17 09:47:21 -0700

    demo for apex perm enforcement

---
+++ force-app/main/default/lwc/testPermission/testPermission.js
@@ -0,0 +1,17 @@
+import { LightningElement, wire } from 'lwc';
+import getTestString from "@salesforce/apex/TestPermissionController.getTestString";
+
+export default class TestPermission extends LightningElement {
+    message
+
+    @wire(getTestString)
+    setTestMessage({ error, data }) {
+        if (data) {
+            this.message = data;
+        }
+        if (error) {
+            debugger;
+            this.message = 'Something happened';
+        }
+    }
+}
